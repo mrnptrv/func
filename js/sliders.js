ecb68a20b4bc1042c4b4f25c6c0163e9c0eb0067
marina.kuznetsova <marina.kuznetsova@piano.io>
2020-05-18 15:31:12 +0400

    Fix things according to visual QA, add sliders, update sliders logic, add video

--- js/sliders.js
+++ js/sliders.js
@@ -1,7 +1,8 @@
 // Слайдер в разделе "Пространства"
 
 const roomsSlider = new Glide('.rooms__slider', {
-  type: 'carousel',
+  type: 'slider',
+  rewind: false,
   peek: {
     before: 0,
     after: 100
@@ -30,8 +31,10 @@ roomsSlider.mount();
 // Слайдер в разделе "События"
 
 const eventsSlider = new Glide('.events__slider', {
-  type: 'carousel',
+  type: 'slider',
+  rewind: false,
   perView: 2,
+  bound: true,
   peek: {
     before: 0,
     after: 120
@@ -66,3 +69,51 @@ eventsSlider.on(['mount.after', 'run'], () => {
 });
 
 eventsSlider.mount();
+
+// Слайдер в разделе "ИТ-тусовки"
+
+const organizeSlider = new Glide('.organize__slider', {
+  type: 'slider',
+  rewind: true
+});
+
+organizeSlider.on(['mount.after', 'run'], () => {
+  let index = organizeSlider.index;
+  let slides = document.querySelectorAll('.organize__gallery-item');
+  let cloneSlides = document.querySelectorAll('.organize__gallery-item.glide__slide--clone');
+
+  document.querySelector('#organize-slider-index').innerHTML = index + 1;
+  document.querySelector('#organize-slider-total').innerHTML = slides.length - cloneSlides.length;
+});
+
+organizeSlider.mount();
+
+// Слайдер в разделе "Отзывы"
+
+const feedbackSlider = new Glide('.feedback__slider', {
+  type: 'slider',
+  rewind: false,
+  perView: 1,
+  gap: 22,
+  peek: {
+    before: 0,
+    after: 200
+  },
+  breakpoints: {
+    959: {
+      gap: 0,
+      peek: 0
+    }
+  }
+});
+
+feedbackSlider.on(['mount.after', 'run'], () => {
+  let index = feedbackSlider.index;
+  let slides = document.querySelectorAll('.feedback__item');
+  let cloneSlides = document.querySelectorAll('.feedback__item.glide__slide--clone');
+
+  document.querySelector('#feedback-slider-index').innerHTML = index + 1;
+  document.querySelector('#feedback-slider-total').innerHTML = slides.length - cloneSlides.length;
+});
+
+feedbackSlider.mount();
