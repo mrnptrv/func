bfc2eece57762289b15cae76513a429269241ffa
marina.kuznetsova <marina.kuznetsova@piano.io>
2020-05-18 18:47:58 +0400

    Add disabled states for arrow buttons

--- js/sliders.js
+++ js/sliders.js
@@ -1,3 +1,19 @@
+// Логика для назначения неактивной кнопки-стрелки
+
+const assignDisabledArrowButton = (slider, slidesAmount, leftArrow, rightArrow) => {
+  if (slider.index === 0) {
+    leftArrow.disabled = true;
+  } else {
+    leftArrow.disabled = false;
+  }
+
+  if (slider.index === (slidesAmount - 1)) {
+    rightArrow.disabled = true;
+  } else {
+    rightArrow.disabled = false;
+  }
+}
+
 // Слайдер в разделе "Пространства"
 
 const roomsSlider = new Glide('.rooms__slider', {
@@ -17,13 +33,17 @@ const roomsSlider = new Glide('.rooms__slider', {
 
 roomsSlider.on(['mount.after', 'run'], () => {
   let index = roomsSlider.index;
-  let slides = document.querySelectorAll('.rooms__slide');
-  let cloneSlides = document.querySelectorAll('.rooms__slide.glide__slide--clone');
-  let caption = slides[index].querySelector('img').alt;
+  const slider = document.querySelector('.rooms__slider');
+  const slides = document.querySelectorAll('.rooms__slide');
+  const caption = slides[index].querySelector('img').alt;
+  const leftArrow = slider.querySelector('.slider-control--left');
+  const rightArrow = slider.querySelector('.slider-control--right');
 
   document.querySelector('#rooms-slider-index').innerHTML = index + 1;
-  document.querySelector('#rooms-slider-total').innerHTML = slides.length - cloneSlides.length;
+  document.querySelector('#rooms-slider-total').innerHTML = slides.length;
   document.querySelector('.rooms__caption').innerHTML = caption;
+
+  assignDisabledArrowButton(roomsSlider, slides.length, leftArrow, rightArrow);
 });
 
 roomsSlider.mount();
@@ -61,11 +81,15 @@ const eventsSlider = new Glide('.events__slider', {
 
 eventsSlider.on(['mount.after', 'run'], () => {
   let index = eventsSlider.index;
-  let slides = document.querySelectorAll('.events__slide');
-  let cloneSlides = document.querySelectorAll('.events__slide.glide__slide--clone');
+  const slider = document.querySelector('.events__slider');
+  const slides = document.querySelectorAll('.events__slide');
+  const leftArrow = slider.querySelector('.slider-control--left');
+  const rightArrow = slider.querySelector('.slider-control--right');
 
   document.querySelector('#events-slider-index').innerHTML = index + 1;
-  document.querySelector('#events-slider-total').innerHTML = slides.length - cloneSlides.length;
+  document.querySelector('#events-slider-total').innerHTML = slides.length;
+
+  assignDisabledArrowButton(eventsSlider, slides.length, leftArrow, rightArrow);
 });
 
 eventsSlider.mount();
@@ -79,11 +103,15 @@ const organizeSlider = new Glide('.organize__slider', {
 
 organizeSlider.on(['mount.after', 'run'], () => {
   let index = organizeSlider.index;
-  let slides = document.querySelectorAll('.organize__gallery-item');
-  let cloneSlides = document.querySelectorAll('.organize__gallery-item.glide__slide--clone');
+  const slider = document.querySelector('.organize__slider');
+  const slides = document.querySelectorAll('.organize__gallery-item');
+  const leftArrow = slider.querySelector('.slider-control--left');
+  const rightArrow = slider.querySelector('.slider-control--right');
 
   document.querySelector('#organize-slider-index').innerHTML = index + 1;
-  document.querySelector('#organize-slider-total').innerHTML = slides.length - cloneSlides.length;
+  document.querySelector('#organize-slider-total').innerHTML = slides.length;
+
+  assignDisabledArrowButton(organizeSlider, slides.length, leftArrow, rightArrow);
 });
 
 organizeSlider.mount();
@@ -109,11 +137,15 @@ const feedbackSlider = new Glide('.feedback__slider', {
 
 feedbackSlider.on(['mount.after', 'run'], () => {
   let index = feedbackSlider.index;
-  let slides = document.querySelectorAll('.feedback__item');
-  let cloneSlides = document.querySelectorAll('.feedback__item.glide__slide--clone');
+  const slider = document.querySelector('.feedback__slider');
+  const slides = document.querySelectorAll('.feedback__item');
+  const leftArrow = slider.querySelector('.slider-control--left');
+  const rightArrow = slider.querySelector('.slider-control--right');
 
   document.querySelector('#feedback-slider-index').innerHTML = index + 1;
-  document.querySelector('#feedback-slider-total').innerHTML = slides.length - cloneSlides.length;
+  document.querySelector('#feedback-slider-total').innerHTML = slides.length;
+
+  assignDisabledArrowButton(feedbackSlider, slides.length, leftArrow, rightArrow);
 });
 
 feedbackSlider.mount();
